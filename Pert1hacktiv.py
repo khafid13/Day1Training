b5d5bdd7bda99fdffa4d71e5c33891487c293655
khafid13 <75995368+khafid13@users.noreply.github.com>
2020-12-15 18:47:43 +0700

    Add files via upload

---
+++ Pert1hacktiv.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+text="hai"
+print(text)
+
+
+# In[2]:
+
+
+text="Kelas Python"
+print(text)
+text2="BMKG"
+print(text + " " + text2)
+
+
+# In[13]:
+
+
+a = 3
+b = 4
+c = a*b
+print(c)
+
+
+# In[4]:
+
+
+print (a+b)
+
+
+# In[5]:
+
+
+print(text + str(c))
+
+
+# In[6]:
+
+
+print(text + " " + str(c))
+
+
+# In[8]:
+
+
+text3 = text + " " + text2 + " " + str(a+b+c)
+print(text3)
+
+
+# In[17]:
+
+
+import math as m
+d=16
+e=m.sqrt(d)
+print(e)
+
+
+# In[22]:
+
+
+f=int(input())
+print(e+f)
+
+
+# In[ ]:
+
+
+
+
